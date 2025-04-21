b7028e7f49afc036ac332ae778bf5bf440455dca
macmini <rubin@macm4.com>
2025-04-21 11:30:44 +0800

    fix pigz

--- armbian_patch/tools/armbian_rootfs.sh
+++ armbian_patch/tools/armbian_rootfs.sh
@@ -94,7 +94,8 @@ redo_rootfs() {
     [[ -n "${image_file}" ]] && {
         cd ${image_path}/
         # mv -f ${image_file} ${image_save_name}
-        pigz -qf *.img || gzip -qf *.img
+        # pigz -qf *.img || gzip -qf *.img
+	pigz -qf *.img | split -b 2000M - *.img.gz.part || gzip -qf *.img | split -b 2000M - *.img.gz.part
         [[ "${?}" == "0" ]] && echo -e "${INFO} 03. Renaming Armbian image completed." || error_msg "03. Failed to rename the image!"
     } || error_msg "03. Failed to find Armbian image!"
 
