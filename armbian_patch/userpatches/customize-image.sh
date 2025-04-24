ff01e91a0ff1d5d4ed6d33dfd01d088df02ffc9b
macmini <rubin@macm4.com>
2025-04-24 17:52:59 +0800

    fix

--- armbian_patch/userpatches/customize-image.sh
+++ armbian_patch/userpatches/customize-image.sh
@@ -68,11 +68,11 @@ LANG=zh_CN.UTF-8" > /etc/default/locale'
 	update-locale
 	# add qq wechat wps
         wget https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_arm64.deb -O /tmp/wechat.deb
-        dpkg -i /tmp/wechat.deb
+        apt install -y /tmp/wechat.deb
         wget https://down.qq.com/qqweb/LinuxQQ/linuxqq_2.0.0-b2-1089_arm64.deb -O /tmp/qq.deb
-        dpkg -i /tmp/qq.deb
+        apt install -y /tmp/qq.deb
         wget https://wps-linux-365.wpscdn.cn/wps/download/ep/Linux365/20327/wps-office_12.8.2.20327.AK.preload.sw_arm64.deb -O /tmp/wps.deb
-        dpkg -i /tmp/wps.deb
+        apt install -y /tmp/wps.deb
 	ls -l /tmp/
         rm -rf /tmp/qq.deb
         rm -rf /tmp/wps.deb
